589d2a8098eec44d2758a14a2e9094d2d65eb09c
polojerry <polojerry4@gmail.com>
2020-03-25 19:34:41 +0300

    Created intro files[fragments] and [hostFragment] that will be used to inform the user about the app

---
+++ app/src/main/java/com/polotechnologies/delvis/uiHosts/IntroHostFragment.kt
@@ -0,0 +1,24 @@
+package com.polotechnologies.delvis.uiHosts
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.polotechnologies.delvis.R
+
+/**
+ * A [Fragment] to host intro fragments.
+ */
+class IntroHostFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_intro_host, container, false)
+    }
+
+}
