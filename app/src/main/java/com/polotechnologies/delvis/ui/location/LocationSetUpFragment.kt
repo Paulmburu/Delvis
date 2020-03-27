1f9f6134befc65df1aaabc97cc49468ab90af337
polojerry <polojerry4@gmail.com>
2020-03-27 09:35:17 +0300

    Added AutoCompleteMaps Fragment to be used to select location address

--- app/src/main/java/com/polotechnologies/delvis/ui/location/LocationSetUpFragment.kt
+++ app/src/main/java/com/polotechnologies/delvis/ui/location/LocationSetUpFragment.kt
@@ -1,19 +1,25 @@
 package com.polotechnologies.delvis.ui.location
 
+import android.R.attr
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
-import androidx.appcompat.app.AlertDialog
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import com.google.android.libraries.places.api.Places
+import com.google.android.libraries.places.api.model.Place
+import com.google.android.libraries.places.widget.Autocomplete
+import com.google.android.libraries.places.widget.AutocompleteActivity
+import com.google.android.libraries.places.widget.model.AutocompleteActivityMode
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
-
 import com.polotechnologies.delvis.R
 import com.polotechnologies.delvis.databinding.FragmentLocationSetUpBinding
-import com.polotechnologies.delvis.databinding.FragmentLoginBinding
+
 
 /**
  * A [Fragment] used to setup the location of the signedUp user.
@@ -21,6 +27,11 @@ import com.polotechnologies.delvis.databinding.FragmentLoginBinding
 class LocationSetUpFragment : Fragment() {
 
     private lateinit var mBinding: FragmentLocationSetUpBinding
+
+    companion object {
+        val AUTOCOMPLETE_REQUEST_CODE = 1
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -28,6 +39,9 @@ class LocationSetUpFragment : Fragment() {
         // Inflate the layout for this fragment
         mBinding =
             DataBindingUtil.inflate(inflater, R.layout.fragment_location_set_up, container, false)
+
+        Places.initialize(context!!, getString(R.string.google_maps_key));
+
         mBinding.btnSetLocation.setOnClickListener {
             displayLocationDialog()
         }
@@ -36,8 +50,10 @@ class LocationSetUpFragment : Fragment() {
 
     private fun displayLocationDialog() {
         val pictureDialogItems = arrayOf("Current Location", "Add an Address")
-        val locationSelectionDialog = MaterialAlertDialogBuilder(context,
-            R.style.MaterialAlertDialog_MaterialComponents_Title_Icon).let { dialogBuilder ->
+        val locationSelectionDialog = MaterialAlertDialogBuilder(
+            context,
+            R.style.MaterialAlertDialog_MaterialComponents_Title_Icon
+        ).let { dialogBuilder ->
             dialogBuilder.setTitle("Select Delivery Address")
             dialogBuilder.setItems(pictureDialogItems) { _, which ->
                 when (which) {
@@ -52,5 +68,36 @@ class LocationSetUpFragment : Fragment() {
         locationSelectionDialog.show()
     }
 
+    private fun loadAddressPicker() {
+        val fields =
+            listOf(Place.Field.ID, Place.Field.NAME)
 
+        val intent =
+            Autocomplete.IntentBuilder(AutocompleteActivityMode.FULLSCREEN, fields).build(context!!)
+        startActivityForResult(intent, AUTOCOMPLETE_REQUEST_CODE)
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == AUTOCOMPLETE_REQUEST_CODE) {
+            when (resultCode) {
+                Activity.RESULT_OK -> {
+                    val place = Autocomplete.getPlaceFromIntent(data!!)
+                }
+                AutocompleteActivity.RESULT_ERROR -> {
+                    val status = Autocomplete.getStatusFromIntent(data!!)
+                    Toast.makeText(context, status.statusMessage, Toast.LENGTH_SHORT).show()
+
+                }
+                AutocompleteActivity.RESULT_CANCELED -> {
+                    Toast.makeText(context, "Cancelled by user", Toast.LENGTH_SHORT).show()
+                }
+
+            }
+
+        }
+
+    }
 }
--- app/src/main/java/com/polotechnologies/delvis/ui/location/googleMapFragment/MapFragment.kt
+++ app/src/main/java/com/polotechnologies/delvis/ui/location/googleMapFragment/MapFragment.kt
@@ -1,27 +1,46 @@
 package com.polotechnologies.delvis.ui.location.googleMapFragment
 
+import android.app.SearchManager
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.appcompat.widget.SearchView
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import com.google.android.gms.common.api.Status
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.model.PointOfInterest
+import com.google.android.libraries.places.api.Places
+import com.google.android.libraries.places.api.model.*
+import com.google.android.libraries.places.api.net.FindAutocompletePredictionsRequest
+import com.google.android.libraries.places.api.net.PlacesClient
+import com.google.android.libraries.places.widget.Autocomplete
+import com.google.android.libraries.places.widget.AutocompleteSupportFragment
+import com.google.android.libraries.places.widget.listener.PlaceSelectionListener
+import com.google.android.libraries.places.widget.model.AutocompleteActivityMode
 import com.polotechnologies.delvis.R
 import com.polotechnologies.delvis.databinding.FragmentMapBinding
+import java.util.*
 
 
 /**
  * A [Fragment] to host google maps.
  */
-class MapFragment : Fragment(), OnMapReadyCallback {
+class MapFragment : Fragment(), PlaceSelectionListener {
 
     private lateinit var mBinding: FragmentMapBinding
+    private lateinit var placesClient: PlacesClient
+    private lateinit var autoCompleteFragment: AutocompleteSupportFragment
+
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -30,19 +49,30 @@ class MapFragment : Fragment(), OnMapReadyCallback {
         mBinding =
             DataBindingUtil.inflate(inflater, R.layout.fragment_map, container, false)
 
-        val mapFragment = childFragmentManager.findFragmentById(R.id.map_user_location) as SupportMapFragment
-        mapFragment.getMapAsync(this)
+        initializePlaces()
 
         return mBinding.root
     }
 
-    override fun onMapReady(googleMap: GoogleMap?) {
-        val sydney = LatLng(-33.852, 151.211)
-        googleMap!!.addMarker(
-            MarkerOptions().position(sydney)
-                .title("Marker in Sydney")
-        )
-        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    private fun initializePlaces() {
+        if (!Places.isInitialized()) {
+            Places.initialize(context!!, getString(R.string.google_maps_key))
+            placesClient = Places.createClient(context!!)
+        }
+        autoCompleteFragment = childFragmentManager.findFragmentById(R.id.autocomplete_fragment) as AutocompleteSupportFragment
+        autoCompleteFragment.setPlaceFields(listOf(Place.Field.ID, Place.Field.NAME, Place.Field.ADDRESS))
+        autoCompleteFragment.setCountry("KE")
+        autoCompleteFragment.setHint("Address, Location")
+        autoCompleteFragment.setOnPlaceSelectedListener(this)
     }
+
+    override fun onPlaceSelected(place: Place) {
+        Toast.makeText(context, "Selected: ${place.address}", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onError(status: Status) {
+        Toast.makeText(context, "Error : ${status.statusMessage}", Toast.LENGTH_SHORT).show()
+    }
+
 
 }
